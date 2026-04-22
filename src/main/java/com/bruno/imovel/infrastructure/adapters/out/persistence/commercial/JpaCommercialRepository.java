d71d7ce07755f449686f9449b079f7591325868e
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2026-04-22 16:28:56 -0300

    feat: update repositories to use entity types for commercial and residential properties

--- src/main/java/com/bruno/imovel/infrastructure/adapters/out/persistence/commercial/JpaCommercialRepository.java
+++ src/main/java/com/bruno/imovel/infrastructure/adapters/out/persistence/commercial/JpaCommercialRepository.java
@@ -1,12 +1,13 @@
 package com.bruno.imovel.infrastructure.adapters.out.persistence.commercial;
 
 import com.bruno.imovel.domain.property.types.CommercialProperty;
+import com.bruno.imovel.infrastructure.adapters.out.persistence.commercial.entity.CommercialPropertyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaCommercialRepository extends JpaRepository<CommercialProperty, Long>,
-                                                 QuerydslPredicateExecutor<CommercialProperty> {
+public interface JpaCommercialRepository extends JpaRepository<CommercialPropertyEntity, Long>,
+                                                 QuerydslPredicateExecutor<CommercialPropertyEntity> {
 
 }
