fa35458de848c8aaf11d481f6cb3919280d5dfa5
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2026-04-22 19:09:44 -0300

    fix: change the type of price in Property to BigDecimal

--- src/main/java/com/bruno/imovel/domain/property/core/Property.java
+++ src/main/java/com/bruno/imovel/domain/property/core/Property.java
@@ -5,13 +5,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import java.math.BigDecimal;
+
 @SuperBuilder
 @NoArgsConstructor
 @Getter
 @Setter
 public abstract class Property {
     private Long id;
-    private Double price;
+    private BigDecimal price;
     private Double totalArea;
     private Localization localization;
     private PropertyStatus propertyStatus;
